c9b2386836bee448d92cc43f0fe3f50aab14df1d
unknown <kazuki1002003@yahoo.com>
2018-07-06 16:12:19 -0700

    adds initial project files

---
+++ main.js
@@ -0,0 +1,108 @@
+
+// Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+  ];
+  
+  // We're going to solve this by breaking the problem into three parts.
+  // Programmers like automating things, we'll populate the HTML drop down options using code,
+  // instead of having to type out all the values.
+  // Create a function that does the some math and gives us the new weight.
+  // Then create a function that responds when the user clicks on the button.
+  
+  // 1. Populate the dropdown element with the data found in the planets array.
+  // The value of each option should be the planet's name.
+  // Use the following built-in methods:
+  // `.forEach` `document.createElement` `document.getElementById` `.appendChild`
+  
+  
+  
+  function calculateWeight(weight, planetName) {
+    // 2. Write the code to return the correct weight
+
+    return weight * planetName;
+  
+  }
+  
+  function handleClickEvent(e) {
+    // 3. Create a variable called userWeight and assign the value of the user's weight.
+    // 4. Create a variable called planetName and assign the name of the selected planet from the drop down.
+    // 5. Create a variable called result and assign the value of the new calculated weight.
+    var userWeight = document.getElementById('user-weight').value;
+    var planetName = document.getElementById('planets').value;
+    var result ='Hello World';
+    var gravity = 0;
+    var totalWeight =10;
+
+    //switch statement to get
+
+    switch(planetName){
+
+        case 'Pluto':
+        gravity = 0.06;
+        break;
+
+        case 'Neptune':
+        gravity = 1.148;
+        break;
+
+        case 'Uranus':
+        gravity = 0.917;
+        break;
+
+        case 'Saturn':
+        gravity = 1.139;
+        break;
+
+        case 'Jupiter':
+        gravity = 2.640;
+        break;
+
+        case 'Mars':
+        gravity = 0.3895;
+        break;
+
+        case 'Moon':
+        gravity = 0.1655;
+        break;
+
+        case 'Earth':
+        gravity = 1;
+        break;
+
+        case 'Venus':
+        gravity = 0.9032;
+        break;
+
+        case 'Mercury':
+        gravity = 0.377;
+        break;
+
+        case 'Sun':
+        gravity = 27.9;
+        break;
+    }
+
+    totalWeight = calculateWeight(userWeight, gravity); //gets the total weight
+
+    result = 'If you were on ' + planetName+', you would weigh ' + totalWeight +'lbs!';
+
+    // 6. Write code to display the message shown in the screenshot.
+  document.getElementById('output').innerHTML = result;
+  }
+  
+  // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
+  document.getElementById('calculate-button').addEventListener('click',handleClickEvent);
+  // Bonus Challenges
+  // 8. Reverse the drop down order so that the sun is first.
+  // 9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)
